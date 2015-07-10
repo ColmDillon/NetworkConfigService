e460ae598a46098cfdbedb5bebd3f457e2fee80c
ecoldil <colm.dillon@ericsson.com>
2015-07-10 15:12:52 +0100

    added CellServiceLocal.java

---
+++ src/main/java/com/networkConfigService/serviceLocal/CellServiceLocal.java
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------------
+ *******************************************************************************
+ * COPYRIGHT Ericsson 2012
+ *
+ * The copyright to the computer program(s) herein is the property of
+ * Ericsson Inc. The programs may be used and/or copied only with written
+ * permission from Ericsson Inc. or in accordance with the terms and
+ * conditions stipulated in the agreement/contract under which the
+ * program(s) have been supplied.
+ *******************************************************************************
+ *----------------------------------------------------------------------------*/
+package com.networkConfigService.serviceLocal;
+
+public interface CellServiceLocal {
+	public void deleteCell(Integer cell_id);
+
+	public void editCell(Integer cell_id, String cell_name, Integer cell_range);
+}
