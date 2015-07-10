ddb12072bfc5bc1b94b0ded26243572cdbd55912
egiovbr <giovanni.brunelli@ericsson.com>
2015-07-10 12:15:18 +0100

    classpath and project added

--- src/main/java/com/networkConfigService/entities/Node.java
+++ src/main/java/com/networkConfigService/entities/Node.java
@@ -1,5 +1,45 @@
 package com.networkConfigService.entities;
 
-public class Node {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@SuppressWarnings("serial")
+@Entity(name="network_node")
+public class Node implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column
+	private long id;
+	
+	@Column
+	private String nodeId;
+	
+	@Column
+	private String location;
+	
+	@Column
+	private double latitude;
+	
+	@Column
+	private double longtitude;
+	
+	private List<Cell> nodeCells = new ArrayList<Cell>();
+	
+	
+	public Node(){
+			
+	}
+	
+	
+	
+	
 
 }
