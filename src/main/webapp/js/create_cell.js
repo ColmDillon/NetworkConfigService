2b97525d28cacbeba5ba54f16d4e2c09e9971f1d
ecoldil <colm.dillon@ericsson.com>
2015-07-13 10:34:51 +0100

    cell html and js created

---
+++ src/main/webapp/js/create_cell.js
@@ -0,0 +1,23 @@
+funtion createCell {
+	var cell_id = document.getElementById("input_cellid").value;
+	var cell_name = document.getElementById("input_cellname").value;
+	var cell_range = document.getElementById("input_cellrange").value;
+	
+	$.ajax({
+		type : 'POST',
+		url : "http://localhost:8080/TrackBackup/rest/cell/create_cell",
+		dataType : "json",
+		data : {
+			"cell_id" : cell_id,
+			"cell_name" : cell_name,
+			"cell_range" : cell_range,
+ 		},
+		success : function(response) {
+			$.each(response, function(i, item) {
+				document.write("Cell created")
+			})
+		}
+	});
+	
+	
+}
